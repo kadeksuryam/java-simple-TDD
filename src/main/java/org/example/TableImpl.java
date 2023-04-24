066b758bd6e7397ee98e75794b7d6b5617e891b1
Kadek Surya Mahardika <kadeksuryam@gmail.com>
2023-04-24 16:21:32 +0700

    now, lets change the implementation using HashTable

--- src/main/java/org/example/TableImpl.java
+++ src/main/java/org/example/TableImpl.java
@@ -1,8 +1,20 @@
 package org.example;
 
+import java.util.HashMap;
+
 public class TableImpl implements Table{
+    private final HashMap<Integer, Record> table;
+
+    public TableImpl() {
+        table = new HashMap<>();
+    }
+
     @Override
     public boolean insertRecord(Record record) {
-        return false;
+        if (table.containsKey(record.getId())) {
+            return false;
+        }
+        table.put(record.getId(), record);
+        return true;
     }
 }
